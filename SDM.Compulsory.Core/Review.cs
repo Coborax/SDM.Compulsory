355af45b5d5f655960b195180f521c7ea330d690
Mikkel Mouridsen <mlm@coborax.com>
2021-09-27 10:55:43 +0200

    Added business entity

---
+++ SDM.Compulsory.Core/Review.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDM.Compulsory.Core
+{
+    public class Review
+    {
+        public int Reviewer { get; set; }
+        public int Movie { get; set; }
+        public int Grade { get; set; }
+        public DateTime ReviewDate { get; set; }
+    }
+}
